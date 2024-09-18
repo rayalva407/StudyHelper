ec7de5a1591a6d7d0281ab7e844f42c4acc73b29
Raymundo Alva <ralv156@wgu.edu>
2024-09-18 09:28:51 -0600

    Sets up layout files

--- app/src/main/java/com/rayalva/studyhelper/viewmodel/QuestionListViewModel.java
+++ app/src/main/java/com/rayalva/studyhelper/viewmodel/QuestionListViewModel.java
@@ -1,4 +1,19 @@
 package com.rayalva.studyhelper.viewmodel;
 
+import android.app.Application;
+import com.rayalva.studyhelper.model.Question;
+import com.rayalva.studyhelper.repo.StudyRepository;
+import java.util.List;
+
 public class QuestionListViewModel {
+
+    private StudyRepository studyRepo;
+
+    public QuestionListViewModel(Application application) {
+        studyRepo = StudyRepository.getInstance(application.getApplicationContext());
+    }
+
+    public List<Question> getQuestions(long subjectId) {
+        return studyRepo.getQuestions(subjectId);
+    }
 }
--- app/src/main/java/com/rayalva/studyhelper/viewmodel/SubjectListViewModel.java
+++ app/src/main/java/com/rayalva/studyhelper/viewmodel/SubjectListViewModel.java
@@ -1,4 +1,23 @@
 package com.rayalva.studyhelper.viewmodel;
 
+import android.app.Application;
+import com.rayalva.studyhelper.model.Subject;
+import com.rayalva.studyhelper.repo.StudyRepository;
+import java.util.List;
+
 public class SubjectListViewModel {
+
+    private StudyRepository studyRepo;
+
+    public SubjectListViewModel(Application application) {
+        studyRepo = StudyRepository.getInstance(application.getApplicationContext());
+    }
+
+    public List<Subject> getSubjects() {
+        return studyRepo.getSubjects();
+    }
+
+    public void addSubject(Subject subject) {
+        studyRepo.addSubject(subject);
+    }
 }
