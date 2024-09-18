6e9561a3fce4c02b707ed485f2d6c3b61562052c
Raymundo Alva <ralv156@wgu.edu>
2024-09-17 22:32:09 -0600

    Implements models

--- app/src/main/java/com/rayalva/studyhelper/model/Question.java
+++ app/src/main/java/com/rayalva/studyhelper/model/Question.java
@@ -1,4 +1,41 @@
 package com.rayalva.studyhelper.model;
 
 public class Question {
+
+    private long mId;
+    private String mText;
+    private String mAnswer;
+    private long mSubjectId;
+
+    public void setId(long id) {
+        mId = id;
+    }
+
+    public long getId() {
+        return mId;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public String getAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(String answer) {
+        mAnswer = answer;
+    }
+
+    public long getSubjectId() {
+        return mSubjectId;
+    }
+
+    public void setSubjectId(long subjectId) {
+        mSubjectId = subjectId;
+    }
 }
--- app/src/main/java/com/rayalva/studyhelper/model/Subject.java
+++ app/src/main/java/com/rayalva/studyhelper/model/Subject.java
@@ -1,4 +1,39 @@
 package com.rayalva.studyhelper.model;
 
+import androidx.annotation.NonNull;
+
 public class Subject {
+
+    private long mId;
+    private String mText;
+    private long mUpdateTime;
+
+    public Subject(@NonNull String text) {
+        mText = text;
+        mUpdateTime = System.currentTimeMillis();
+    }
+
+    public long getId() {
+        return mId;
+    }
+
+    public void setId(long id) {
+        mId = id;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String subject) {
+        mText = subject;
+    }
+
+    public long getUpdateTime() {
+        return mUpdateTime;
+    }
+
+    public void setUpdateTime(long updateTime) {
+        mUpdateTime = updateTime;
+    }
 }
